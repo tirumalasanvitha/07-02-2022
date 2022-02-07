ce3df438401b79f9ae8985ad3c185277fd26664b
DELL <DELL@192.168.2.11>
2022-02-07 10:50:47 +0530

    javaVariable program is added into the 07-02-2022 project

---
+++ 07-02-2022/src/com/javaVariables/Example1.java
@@ -0,0 +1,33 @@
+package com.javaVariables;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+	// write a java program to print student information
+		int studentId;
+		String studentName;
+		float studentMarks;
+		String studentAddress;
+		//variable intialization
+		studentId=3020;
+		studentName="sanvitha";
+		studentMarks=54.82f;
+		studentAddress="Nizamabad";
+		
+		System.out.println("studentId:");
+		System.out.println(studentId);
+		
+		System.out.println("studentName:");
+		System.out.println(studentName);
+		
+		System.out.println("studentMarks:");
+		System.out.println(studentMarks);
+		
+		System.out.println("studentAddress:");
+		System.out.println(studentAddress);
+		
+		
+
+	}
+
+}
