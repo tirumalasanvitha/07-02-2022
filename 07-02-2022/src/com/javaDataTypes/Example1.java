3538a6ad23dee7bb6bf9f449a1e7e1feb888b10e
DELL <DELL@192.168.2.11>
2022-02-07 22:32:32 +0530

    commit DataTypes project into 07-02-2022

---
+++ 07-02-2022/src/com/javaDataTypes/Example1.java
@@ -0,0 +1,11 @@
+package com.javaDataTypes;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		int age=25;
+		System.out.println(age);
+
+	}
+
+}
---
+++ 07-02-2022/src/com/javaDataTypes/Example2.java
@@ -0,0 +1,22 @@
+package com.javaDataTypes;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		
+	
+		//int employeeids=387,286,846,675,826;
+		//System.out.println(employeeids);
+		
+		
+		int employeeids[]= {387,286,846,675,826};
+		//System.out.println(employeeids[]);
+		
+		for(int employeeid: employeeids)
+		{
+			System.out.println(employeeid);
+		}
+		
+	}
+
+}
